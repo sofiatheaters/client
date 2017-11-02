f9fe782830a9d8fee990f4e535e0d8f34445768d
hukuuu <hukuuu@gmail.com>
2017-11-02 17:59:53 +0200

    navbar tweaks

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,56 @@
 import React from 'react'
+import {
+  Menu,
+  MenuItem,
+  MenuDivider,
+  Popover,
+  Button,
+  Position
+} from '@blueprintjs/core'
+
+const noop = () => {}
+
+const ScheduleMenu = (
+  <Menu >
+    <MenuItem onClick={noop} text='Day'/>
+    <MenuItem onClick={noop} text='Week'/>
+    <MenuItem onClick={noop} text='Month'/>
+    <MenuItem onClick={noop} text='Two Months'/>
+  </Menu>
+)
+const SchedulePopover = () => (
+  <Popover content={ScheduleMenu} position={Position.BOTTOM}>
+    <button className='pt-button pt-minimal pt-icon-timeline-events'>Schedule</button>
+  </Popover>
+)
+
+const ProfileMenu = (
+  <Menu >
+    <MenuItem iconName='pt-icon-log-out' onClick={noop} text='Logout'/>
+  </Menu>
+)
+
+const ProfilePopover = () => (
+  <Popover content={ProfileMenu} position={Position.BOTTOM}>
+    <button className='pt-button pt-minimal pt-icon-user'></button>
+  </Popover>
+)
 
 const Navbar = () => (
-  <nav className="pt-navbar pt-dark pt-fixed-top">
-    <div className="pt-navbar-group pt-align-left">
-      <div className="pt-navbar-heading">Blueprint</div>
-      <input className="pt-input" placeholder="Search files..." type="text" />
+  <nav className='pt-navbar pt-dark pt-fixed-top'>
+    <div className='pt-navbar-group pt-align-left'>
+      <div className='pt-navbar-heading'>Theaters app</div>
+      <span className='pt-navbar-divider'></span>
+      <input className='pt-input' placeholder='Search plays...' type='text' />
+      <span className='pt-navbar-divider'></span>
+      <button className='pt-button pt-minimal pt-icon-tick-circle'>All</button>
+      <SchedulePopover />
+      <button className='pt-button pt-minimal pt-icon-bookmark'>Saved</button>
+      
     </div>
-    <div className="pt-navbar-group pt-align-right">
-      <button className="pt-button pt-minimal pt-icon-home">Home</button>
-      <button className="pt-button pt-minimal pt-icon-document">Files</button>
-      <span className="pt-navbar-divider"></span>
-      <button className="pt-button pt-minimal pt-icon-user"></button>
-      <button className="pt-button pt-minimal pt-icon-notifications"></button>
-      <button className="pt-button pt-minimal pt-icon-cog"></button>
+    <div className='pt-navbar-group pt-align-right'>
+      <span className='pt-navbar-divider'></span>
+      <ProfilePopover />
     </div>
   </nav>
 )
