d44352c9f6a9b5ab84967e79d3a323c81759f0a0
hukuuu <hukuuu@gmail.com>
2017-11-07 09:16:24 +0200

    test with new data

--- src/plays/Play.js
+++ src/plays/Play.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import './Play.css'
 
+// const calculateOneToFiveRating = rating => {
+//   const points = rating[1] + 2*rating[2] + 3*rating[3] + 4*rating[4] + 5*rating[5]
+//   const maximum = 5 * (rating[1] + rating[2] + rating[3] + rating[4] + rating[5])
+//   return points / maximum * 5
+// }
+
 const calculateOneToFiveRating = rating => {
-  const points = rating[1] + 2*rating[2] + 3*rating[3] + 4*rating[4] + 5*rating[5]
-  const maximum = 5 * (rating[1] + rating[2] + rating[3] + rating[4] + rating[5])
-  return points / maximum * 5
+  return Math.random() * 5
 }
 
-const imageUrl = 'http://theatre.art.bg/img/photos/INDEX15094552441plakat%20pileto.jpeg'
+// const imageUrl = 'http://theatre.art.bg/img/photos/INDEX15094552441plakat%20pileto.jpeg'
 
 export default ({ play }) => (
   // <img className='play' src='http://via.placeholder.com/500x300/bada55' />
-  <div className='play' style={{'backgroundImage': `url(${imageUrl})`}}>
+  <div className='play' style={{'backgroundImage': `url(${play.imageUrl})`}}>
     <h1>{play.name}</h1>
     <span>{calculateOneToFiveRating(play.rating)}</span>
     <span>{play.theater}</span>
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -3,7 +3,7 @@ export const getFilteredPlays = (allPlays, filter, watched) =>
     ? allPlays
     : allPlays.filter( play =>
       (
-        play.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1
+        play.title.toLowerCase().indexOf(filter.toLowerCase()) !== -1
         || play.director.toLowerCase().indexOf(filter.toLowerCase()) !== -1
         || play.theater.toLowerCase().indexOf(filter.toLowerCase()) !== -1
         || play.actors.filter( actor => actor.toLowerCase().indexOf(filter.toLowerCase()) !== -1).length > 0
